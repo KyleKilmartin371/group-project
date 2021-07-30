8cd3bcd326fccc499869cd8ac03e9cc323a76f74
Snow <84701509+snowden421@users.noreply.github.com>
2021-07-30 13:00:05 -0700

    added weather api call

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,27 @@
 //elements
-
-
+var weatherApiKey = "9f952a484bb0642f0030c770704f46cd"
+var weatherApiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=4717782&appid=";
+var weather = weatherApiUrl + weatherApiKey;
+var city = document.querySelector("#location").value;
+var getCityCode = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=9f952a484bb0642f0030c770704f46cd"
 //function to call weather API
-
 
 // function to call meetup API
 
+
 //function to call both API upon submit click
+var weatherFetch = fetch(getCityCode).then(function(response){
+    if (response.ok) {
+        response.json().then(function(data) {
+            //console.log(data.id);
+            fetch("http://api.openweathermap.org/data/2.5/forecast?id="+data.id+"&appid=9f952a484bb0642f0030c770704f46cd")
+                .then(function(response){
+                    response.json().then(function(data){
+                        console.log(data);
+                    })
+                })
+        });
+    };
+});
+
+document.querySelector("#btn").addEventListener("click", weatherFetch);
