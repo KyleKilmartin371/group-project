01fa0aff6a9ce0999303bbc12fa1d64f5fac77b1
Snow <84701509+snowden421@users.noreply.github.com>
2021-07-30 13:02:54 -0700

    fixed file structure

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,8 @@ var weatherApiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=4717782&
 var weather = weatherApiUrl + weatherApiKey;
 var city = document.querySelector("#location").value;
 var getCityCode = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=9f952a484bb0642f0030c770704f46cd"
+
 //function to call weather API
-
-// function to call meetup API
-
-
-//function to call both API upon submit click
 var weatherFetch = fetch(getCityCode).then(function(response){
     if (response.ok) {
         response.json().then(function(data) {
@@ -23,5 +19,11 @@ var weatherFetch = fetch(getCityCode).then(function(response){
         });
     };
 });
+
+// function to call meetup API
+
+
+//function to call both API upon submit click
+
 
 document.querySelector("#btn").addEventListener("click", weatherFetch);
