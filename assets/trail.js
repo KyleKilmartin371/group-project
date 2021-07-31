661074f713795c9b480b25c483b91ce85821a39d
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-07-30 19:59:27 -0700

    weather code

--- assets/trail.js
+++ assets/trail.js
@@ -1,16 +1,65 @@
+// create a function to store city search terms using localStorage
+function pastCity(city) {
+	localStorage.setItem("city-name", city);
+	
+	var cityStorage = document.createElement ("button");
+	cityStorage.setAttribute("class", "pastCity form-control");
+	cityStorage.setAttribute("id",city);
+	var pastCityResults = document.querySelector("#search");
+	pastCityResults.appendChild(cityStorage);
+	cityStorage.innerHTML=city;
+  };
+  
+  // get city search term and use .value to store its value
+  function myFunction () {
+	var searchTerm = document.querySelector("#searchTerm").value; 
+	event.preventDefault();
+	console.log(searchTerm); 
+	pastCity(searchTerm);
+  
+	// Make a `fetch` request concatenating that search term to the query URL for current weather
+	fetch (
+	  'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+	)  
+	
+	// Converts the response to JSON
+	.then(function(response) {
+	  console.log(response);
+	  return response.json();
+	})  
+  
+	.then(function(response) {
+	  console.log(response.data);
+	
+	// use jQuery to display current date and weather using elements from the response
+	  var currentDate = moment().format("M/D/YYYY").toString();
+	  $("#city-name").html(response.name + " " + currentDate); 
+	  $("#temp").html("Temp: " + response.main.temp + " °F");
+	  $("#speed").html("Wind: " + response.wind.speed + " mph");
+	  $("#humidity").html("Humidity: " + response.main.humidity + "%");
+	  console.log(response.weather[0].icon);
+  
+	  // Make a `fetch` request concatenating that search term to the query URL for weather forecast
+	  fetch (
+		'https://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm +'&units=imperial&appid=8e2a4447de15a5d804cf8a7d25a93eca'
+	  )  
+	
+	  // Converts the response to JSON
+	  .then(function(response) {
+		console.log(response);
+		return response.json();
+	  })  
+  
+	  .then(function(response) {
+		console.log(response.data);
+		
+		/* use jQuery to bring weather data from the API to the 5-day forecast cards */
 
-
-
-fetch("https://trailapi-trailapi.p.rapidapi.com/trails/map/%7Bid%7D/gpx/", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "326703dddfmsh8547e243dd4a687p1f7f2cjsn8e5984fc1842",
-		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+	  })
+	})  
+  }
+  
+  // link button to search result for the city in the button
+  // function buttonClick {
+  
+  // }
